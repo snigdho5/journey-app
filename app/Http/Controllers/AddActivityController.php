f107669f9fa719e3439db0dbe9a029f1b1152c23
Snigdho <snigdhoupadhyay@outlook.com>
2022-03-14 00:19:50 +0530

    changes activity apis

--- app/Http/Controllers/AddActivityController.php
+++ app/Http/Controllers/AddActivityController.php
@@ -7,6 +7,8 @@ use App\Models\AddActivity;
 use Illuminate\Support\Facades\Validator;
 use DateTime;
 use DateInterval;
+use Carbon\Carbon;
+
 
 class AddActivityController extends Controller
 {
@@ -23,21 +25,142 @@ class AddActivityController extends Controller
                 'message' => 'Validation failed!'
             ], 401);
         }
-        $activity = AddActivity::where('user_id', $request->user_id)
-            ->where('activity_date', "$request->activity_date")
-            ->get();
+        // $activity = AddActivity::where('user_id', $request->user_id)
+        //     ->where('activity_date', $request->activity_date)
+        //     ->get();
 
-        if ($activity) {
+        $userActTimeSum = AddActivity::where('user_id', $request->user_id)->where('activity_date', $request->activity_date)->sum('activity_time');
+
+        if (isset($userActTimeSum) && $userActTimeSum > 0) {
+
+            $yes_yes_count = 0;
+            $yes_no_count = 0;
+            $no_yes_count = 0;
+            $no_no_count = 0;
+
+            $yes_yes_per = 0;
+            $yes_no_per = 0;
+            $no_yes_per = 0;
+            $no_no_per = 0;
+
+            $userActTimeSum = AddActivity::where('user_id', $request->user_id)->where('activity_date', $request->activity_date)->sum('activity_time');
+
+            // yes yes
+            $yes_yes_count = AddActivity::where('user_id', $request->user_id)
+                ->where('activity_date', $request->activity_date)
+                ->where('is_important', 'yes')
+                ->where('is_liked', 'yes')
+                ->count('activity_time');
+
+            if ($yes_yes_count > 0) {
+
+                $yes_yes_sum = AddActivity::where('user_id', $request->user_id)
+                    ->where('activity_date', $request->activity_date)
+                    ->where('is_important', 'yes')
+                    ->where('is_liked', 'yes')
+                    ->sum('activity_time');
+                if ($yes_yes_sum > 0) {
+                    $yes_yes_per = (($yes_yes_sum / 1440) * 100);
+                } else {
+                    $yes_yes_per = 0;
+                }
+            } else {
+                $yes_yes_per = 0;
+            }
+
+            //yes no
+            $yes_no_count = AddActivity::where('user_id', $request->user_id)
+                ->where('activity_date', $request->activity_date)
+                ->where('is_important', 'yes')
+                ->where('is_liked', 'no')
+                ->count('activity_time');
+
+            if ($yes_no_count > 0) {
+
+                $yes_no_sum = AddActivity::where('user_id', $request->user_id)
+                    ->where('activity_date', $request->activity_date)
+                    ->where('is_important', 'yes')
+                    ->where('is_liked', 'no')
+                    ->sum('activity_time');
+                if ($yes_no_sum > 0) {
+                    $yes_no_per = (($yes_no_sum / 1440) * 100);
+                } else {
+                    $yes_no_per = 0;
+                }
+            } else {
+                $yes_no_per = 0;
+            }
+
+            //no yes
+            $no_yes_count = AddActivity::where('user_id', $request->user_id)
+                ->where('activity_date', $request->activity_date)
+                ->where('is_important', 'no')
+                ->where('is_liked', 'yes')
+                ->count('activity_time');
+
+            if ($no_yes_count > 0) {
+
+                $no_yes_sum = AddActivity::where('user_id', $request->user_id)
+                    ->where('activity_date', $request->activity_date)
+                    ->where('is_important', 'no')
+                    ->where('is_liked', 'yes')
+                    ->sum('activity_time');
+                if ($no_yes_sum > 0) {
+                    $no_yes_per = (($no_yes_sum / 1440) * 100);
+                } else {
+                    $no_yes_per = 0;
+                }
+            } else {
+                $no_yes_per = 0;
+            }
+
+            // no no
+
+            $no_no_count = AddActivity::where('user_id', $request->user_id)
+                ->where('activity_date', $request->activity_date)
+                ->where('is_important', 'no')
+                ->where('is_liked', 'no')
+                ->count('activity_time');
+
+            if ($no_no_count > 0) {
+
+                $no_no_sum = AddActivity::where('user_id', $request->user_id)
+                    ->where('activity_date', $request->activity_date)
+                    ->where('is_important', 'no')
+                    ->where('is_liked', 'yes')
+                    ->sum('activity_time');
+                if ($no_no_sum > 0) {
+                    $no_no_per = (($no_no_sum / 1440) * 100);
+                } else {
+                    $no_no_per = 0;
+                }
+            } else {
+                $no_no_per = 0;
+            }
+
+
             $response = [
                 'status' => 1,
                 'message' => 'Activitites found!',
-                'respData' => $activity
+                'respData' => [
+                    'total_mins' => $userActTimeSum,
+                    'yes_yes_count' => $yes_yes_count,
+                    'yes_no_count' => $yes_no_count,
+                    'no_yes_count' => $no_yes_count,
+                    'no_no_count' => $no_no_count,
+                    'yes_yes_per' => $yes_yes_per,
+                    'yes_no_per' => $yes_no_per,
+                    'no_yes_per' => $no_yes_per,
+                    'no_no_per' => $no_no_per,
+                    // 'activity' => $activity,
+
+                ]
             ];
         } else {
             $response = [
                 'status' => 0,
-                'message' => 'Activity not found!',
-                'respData' => $activity
+                'message' => 'No activities found!',
+                'respData' => []
             ];
         }
         return response($response, 200);
@@ -49,7 +172,7 @@ class AddActivityController extends Controller
             'user_id' => 'required|numeric',
             'activity_id' => 'required|numeric',
             'activity_date' => 'required|date',
-            'activity_time' => 'required',
+            'activity_time' => 'required|integer|between:1,1440',
             'is_important' => 'required|in:yes,no',
             'is_liked' => 'required|in:yes,no'
         ]);
@@ -61,21 +184,41 @@ class AddActivityController extends Controller
             ], 401);
         }
 
-        $userAct = AddActivity::where('user_id', $request->user_id)->get();
+        // $userAct = AddActivity::where('user_id', $request->user_id)->where('activity_date', $request->activity_date)->get();
+
+        $userActSum = AddActivity::where('user_id', $request->user_id)->where('activity_date', $request->activity_date)->sum('activity_time');
         // print_r($userAct);die;
-        if (!empty($userAct)) {
-            $total_hrs = 0;
 
-            // foreach ($userAct as $key => $value) {
-            //     $date = new DateTime($total_hrs);
-            //     $date->add(new DateInterval('PT'.$value->activity_time.'H'));
-            //     echo $date->format('H:i:s a');
-            //     echo $value->activity_time . '<br>';
-            //     // $total_hrs += date('h:i:s A', strtotime('+' . $value->activity_time . ' hours'));
-            // }
+        // $current = Carbon::now();
 
-            // echo $total_hrs;die;
+        if (isset($userActSum) && $userActSum > 0) {
 
+            $addedMins = $userActSum + $request->activity_time;
+            // echo $addedMins;die;
+
+            if ($addedMins < 1440) {
+                $addActivity = new AddActivity();
+                $addActivity->user_id = $request->user_id;
+                $addActivity->activity_id = $request->activity_id;
+                $addActivity->activity_date = $request->activity_date;
+                $addActivity->activity_time = $request->activity_time;
+                $addActivity->is_important = $request->is_important;
+                $addActivity->is_liked = $request->is_liked;
+                $addActivity->save();
+
+                return response([
+                    'status' => 1,
+                    'message' => 'Activity added!',
+                    'respData' => $addActivity
+                ], 200);
+            } else {
+                return response([
+                    'status' => 0,
+                    'message' => 'Total activity time cannot cross the day limit!',
+                    'respData' => []
+                ], 200);
+            }
+        } else {
             $addActivity = new AddActivity();
             $addActivity->user_id = $request->user_id;
             $addActivity->activity_id = $request->activity_id;
@@ -89,12 +232,6 @@ class AddActivityController extends Controller
                 'status' => 1,
                 'message' => 'Activity added!',
                 'respData' => $addActivity
-            ], 200);
-        } else {
-            return response([
-                'status' => 0,
-                'message' => 'Activity already exists!',
-                'respData' => []
             ], 200);
         }
     }
